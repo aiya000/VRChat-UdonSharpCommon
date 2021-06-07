db0039664d943c3ec6174951f9f1da9c0c020ffd
aiya000 <aiya000.develop@gmail.com>
2021-06-07 20:22:48 +0900

    :bug: Fix compile errors

--- Assets/galaxy-sixth-sensey/UdonSharpCommon/ActivateOnce.cs
+++ Assets/galaxy-sixth-sensey/UdonSharpCommon/ActivateOnce.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1068c1493c5c94de6860f2ffe29d419526365673ecf0f36e8ddd497b6493e2c2
-size 1018
+oid sha256:91405840dd0bf85cdac980b3caf0de02c17a9e9a977046fcc0d1d2bfe42ffabb
+size 1013
--- Assets/galaxy-sixth-sensey/UdonSharpCommon/ChangeActiveOnStarted.cs
+++ Assets/galaxy-sixth-sensey/UdonSharpCommon/ChangeActiveOnStarted.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c8f2bfc20940568638db18af545d7e06abd5beff3c2f94cf0d47f6256edb9a2
-size 1739
+oid sha256:0f3320efffefd2bc03f3a43c70ca02d0133e538a0bc287b9512b8630a7031954
+size 1773
