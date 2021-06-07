78e39bb0eb671d37ffe7572bb316f73324aeaa9e
aiya000 <aiya000.develop@gmail.com>
2021-06-07 20:37:20 +0900

    :up: Import VRCHierarchyHighlighter_v2020.12.26.0

---
+++ Assets/VRCHierarchyHighlighter/Editor/VRCHierarchyHighlighter.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f870cb5ee263c7a78a66d22534f2a15b659e46c6bd6a67211b1984c930cbf9a0
+size 16541
---
+++ Assets/VRCHierarchyHighlighter/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4de92887f0b554c48e07169cd740a6fcbb853a54db6bb4d0945b9c7248ca9af4
+size 5519
